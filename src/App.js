eedfa95d179b2d1790128a5841ecdec954ac05de
miruuww <you@example.com>
2024-03-17 14:11:29 +0700

    crud belanja mobil

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
+import ProductCreate from './components/ProductCreate.js';
+import ProductList from './components/ProductList.js';
+import { Products } from './data/Product.js';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState(Products);
+  const editProductById = (id, data) => {
+    const updatedProducts = products.map((prod) => {
+      if (prod.id === id) {
+        console.log({ ...prod, ...data });
+        return { ...prod, ...data };
+      }
+
+      return prod;
+    });
+    console.log(updatedProducts);
+    setProducts(updatedProducts);
+  };
+  const onCreateProduct = (product) => {
+    setProducts([
+      ...products,
+      {
+        id: Math.round(Math.random() * 1998),
+        ...product,
+      },
+    ]);
+    console.log(products);
+  };
+  const onDeleteProduct = (id) => {
+    const updatedProducts = products.filter((prod) => {
+      return prod.id !== id;
+    });
+    setProducts(updatedProducts);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app-title">Belanja Mobil</div>
+      <ProductCreate onCreateProduct={onCreateProduct} />
+      <ProductList
+        onEditProduct={editProductById}
+        products={products}
+        onDeleteProduct={onDeleteProduct}
+      />
+    </>
   );
-}
+};
 
 export default App;
